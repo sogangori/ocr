7b6dd6486aef90255ee2298b8ffc4eef36cef837
sogangori <sogangori@naver.com>
2017-07-18 16:53:10 +0900

    Removing grid cell padding

--- TrainingCharacterExtractor/TrainCharacterExtractor.py
+++ TrainingCharacterExtractor/TrainCharacterExtractor.py
@@ -26,7 +26,18 @@ class TrainCharacterExtractor():
         img = Image.open(path)
         grayImg = img.convert('L')
         self.grayImg = PIL.ImageOps.invert(grayImg)
-        self.grayArr = np.asarray(self.grayImg)        
+        self.grayArr = np.asarray(self.grayImg).copy()        
+
+    def ClearByThreshold(self):
+        shp = self.grayArr.shape
+        delete_count = 0
+        for y in range(shp[0]):
+            for x in range(shp[1]):
+                if self.grayArr[y,x]< 1:
+                    self.grayArr[y,x] = 0;
+                    delete_count+=1
+        print ('ClearByThreshold', shp,'delete_count',delete_count)
+
 
     def GetRotationAngle(self):    
         image = self.grayImg
@@ -59,7 +70,8 @@ class TrainCharacterExtractor():
         print ('max Angle',maxAngle,'maxMean',maxMean)
         if maxAngle!=0:
             self.grayImg = image.rotate(maxAngle)
-            self.grayArr = np.asarray(self.grayImg)
+            self.grayArr = np.asarray(self.grayImg).copy()
+            #self.ClearByThreshold()
 
         self.gridArr = np.asarray(self.grayImg).copy()
 
@@ -95,14 +107,14 @@ class TrainCharacterExtractor():
         pillas = []        
         candidate2Index = 0                
                         
-        for i in range(1,len(deriv)):            
+        for i in range(1,len(deriv)-1):            
             if deriv[i]== 0 and deriv[i+1]==0:
                 continue
             elif sum_col[i]<sum_col_mean/1 and (deriv[i-1]<= 0 and deriv[i]>0):
                 pilla_w = i-i0                
                 pillas.append(pilla_w)
                 list.append(i)
-                print ('axis',axis,candidate2Index, i0,'~', i,'w:', pilla_w)
+                #print ('axis',axis,candidate2Index, i0,'~', i,'w:', pilla_w)
                 i0 = i
                 candidate2Index+=1
                 if axis==1: self.gridArr[i,:] = 255
@@ -138,6 +150,38 @@ class TrainCharacterExtractor():
     def GetGrid(self):  
         self.GetLetterSize(0)
         self.GetLetterSize(1)
+
+    def RemovePadding(self, src):
+        sumRow = np.sum(src, axis=1)
+        sumCol = np.sum(src, axis=0)
+        
+        y0 = 0
+        y1 = len(sumRow)
+        x0 = 0
+        x1 = len(sumCol)
+        for y in range(len(sumRow)):
+            if sumRow[y]>0 : 
+                y0 = y
+                break;
+        for y in range(len(sumRow)):
+            index = len(sumRow)-1-y
+            if sumRow[index]>0 : 
+                y1 = index
+                break;        
+        for x in range(len(sumCol)):
+            if sumCol[x]>0 : 
+                x0 = x
+                break;
+        for x in range(len(sumCol)):
+            index = len(sumCol)-1-x
+            if sumCol[index]>0 : 
+                x1 = index
+                break;
+        dst = src[y0:y1+1,x0:x1+1]
+        print ('sum axis=1',y0,'~',y1,sumRow)
+        print ('sum axis=0',x0,'~',x1,sumCol)
+        print ('cut', y0,y1,x0,x1)
+        return dst
     
     def GetGridCell(self):
         rows = len(self.font_offsets_y)
@@ -147,18 +191,39 @@ class TrainCharacterExtractor():
             plt.figure(self.index_figure)            
             self.index_figure+=1
             subplot_index = 1
-            for y in range(2):
+            lpf = [0,0.1,0.2,0.3,0.4,0.3,0.2,0.1,0]
+            #lpf = [0,0.1,0.2,0.3,0.2,0.1,0]        
+            for y in range(1):
                 for x in range(2):
                     y0 = self.font_offsets_y[y]
                     y1 = self.font_offsets_y[y+1] + 1
                     x0 = self.font_offsets_x[x]
                     x1 = self.font_offsets_x[x+1] + 1
-                    gridCell = self.gridArr[y0:y1,x0:x1]
-                    imgCell = Image.fromarray(gridCell)            
-                    plt.subplot(2,2,subplot_index)
-                    subplot_index+=1
                     print('y0,y1,x0,x1',y0,y1,x0,x1)
-                    plt.title('cell'+str(y)+'/'+str(x))
+                    gridCell = self.grayArr[y0:y1,x0:x1]
+                    cellChar = self.RemovePadding(gridCell)
+                    print ('cellChar',cellChar.shape)
+                    imgCell = Image.fromarray(gridCell)            
+                    imgChar = Image.fromarray(cellChar)
+                    plt.subplot(2,3,subplot_index)
+                    subplot_index+=1
+                    plt.title('cell'+str(y)+'/'+str(x)+str(gridCell.shape))
                     plt.imshow(imgCell, cmap = plt.get_cmap('gray'))
+                    plt.subplot(2,3,subplot_index)
+                    subplot_index+=1
+                    plt.title('cell'+str(y)+'/'+str(x)+str(cellChar.shape))
+                    #plt.imshow(imgChar, cmap = plt.get_cmap('gray'))
+                    sum_row = np.sum(gridCell, axis=0)
+                    sum_row_conv = np.convolve(sum_row,lpf,'same')
+                    plt.plot(sum_row)
+                    plt.plot(sum_row_conv)
+
+                    plt.subplot(2,3,subplot_index)
+                    subplot_index+=1                    
+                    sum_col = np.sum(gridCell, axis=1)
+                    sum_col_conv = np.convolve(sum_col,lpf,'same')
+                    plt.plot(sum_col)
+                    plt.plot(sum_col_conv)
+
         return 0
 
